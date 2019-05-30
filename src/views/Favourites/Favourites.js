6e626f1c6da0a0dd977fd2705dcbd4dea5de9e05
Wiktor Gała <wiktorgal4@gmail.com>
2019-05-30 18:51:27 +0200

    feat(Favourites): favourites view create

---
+++ src/views/Favourites/Favourites.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Favourites extends Component {
+  render() {
+    return (
+        <h2>Favourites</h2>        
+    )
+  }
+}
+
+export default Favourites
