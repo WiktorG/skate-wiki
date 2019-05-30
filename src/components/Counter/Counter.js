abfd29ff2e3b7f9a29b389633da507ebc713a54b
Wiktor Gała <wiktorgal4@gmail.com>
2019-05-30 20:28:52 +0200

    feat(Counter): working redux counter

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react'
-
+import { connect } from 'react-redux';
 import './actions';
+
+import { increaseCounter, decreaseCounter } from './actions';
 
 class Counter extends Component {
   render() {
+    const number = this.props.counter.number;
     return (
-        <h2>Counter</h2>  
+        <div>
+            <h2>Counter is equal {number}</h2>
+            <button onClick={e => { e.preventDefault(e); this.props.increaseCounter() }}>+</button>
+            <button onClick={e => { e.preventDefault(e); this.props.decreaseCounter() }}>-</button>
+        </div>
     )
   }
 }
 
-export default Counter
+export default connect(state => ({
+    counter: state.counter,
+}), dispatch => ({
+    increaseCounter: () => dispatch(increaseCounter()),
+    decreaseCounter: () => dispatch(decreaseCounter()),
+}) )(Counter)
--- src/components/Counter/actions.js
+++ src/components/Counter/actions.js
@@ -1,13 +1,38 @@
  
+import { dispatch } from 'redux';
 import reedux from 'reedux';
 import store from '../../services/store';
+
+const INCREASE_COUNTER = 'INCREASE_COUNTER';
+const DECREASE_COUNTER = 'DECREASE_COUNTER';
 
 const initialState = {
     number: 0
 }
 
+const increaseCounter = params => dispatch => {
+    dispatch({type: INCREASE_COUNTER});
+}
+
+const decreaseCounter = params => dispatch => {
+    dispatch({type: DECREASE_COUNTER});
+}
+
+
+
+
 const counterReducer = (state = initialState, action) => {
     switch (action.type) {
+        case INCREASE_COUNTER: 
+            return {
+                ...state,
+                number: state.number + 1,
+            }
+        case DECREASE_COUNTER: 
+            return {
+                ...state,
+                number: state.number - 1,
+            }
         default: 
             return state;
     }
@@ -16,3 +41,5 @@ const counterReducer = (state = initialState, action) => {
 const storePath = reedux(store)
 const reducer = storePath('counter', initialState);
 reducer(counterReducer);
+
+export { increaseCounter, decreaseCounter }
