b32259e5050ca8f55a1ec834b2c4e7ebc3a2b959
Wiktor Gała <wiktorgal4@gmail.com>
2019-05-30 18:35:09 +0200

    feat(Router): basic router example

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 
-function App() {
+const Home = () => {
+  return (
+    <h2>Home</h2>
+  )
+}
+
+const Favourites = () => {
+  return (
+    <h2>Favourites</h2>
+  )
+}
+
+const NotFound = () => {
+  return (
+    <h2>404</h2>
+  )
+}
+
+const App = () => {
   return (
     <div className="App">
-      <h1>ReactSzn #2</h1>
+      <Router>
+        <h1>Basic router example</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/fav">Favourites</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/fav" component={Favourites} />
+          <Route component={NotFound} />
+        </Switch>
+      </Router>
     </div>
   );
 }
