100ba1f89572e73617ab37e4ce6ea3696a575b7f
Wiktor Gała <wiktorgal4@gmail.com>
2019-05-31 18:53:54 +0200

    refactor: remove redux counter example

--- src/views/Favourites/Favourites.js
+++ src/views/Favourites/Favourites.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { increaseCounter, decreaseCounter } from './../../components/Counter/actions';
 class Favourites extends Component {
   render() {
     return (
       <div>
         <h2>Favourites</h2>   
-        <button onClick={e => { e.preventDefault(e); this.props.increaseCounter() }}>+</button>
-        <button onClick={e => { e.preventDefault(e); this.props.decreaseCounter() }}>-</button>   
       </div>  
     )
   }
@@ -15,6 +12,4 @@ class Favourites extends Component {
 
 export default connect(state => ({
 }), dispatch => ({
-  increaseCounter: () => dispatch(increaseCounter()),
-  decreaseCounter: () => dispatch(decreaseCounter()),
 }) )(Favourites)
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,11 +1,9 @@
 import React, { Component } from 'react'
-import Counter from './../../components/Counter/Counter';
 class Home extends Component {
   render() {
     return (
       <React.Fragment>
         <h2>Home</h2>  
-        <Counter/> 
       </React.Fragment>     
     )
   }
