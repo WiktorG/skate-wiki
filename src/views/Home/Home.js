2be71ebc4cdafa0bcfce9b2798504d98f5aa2a7a
Wiktor Gała <wiktorgal4@gmail.com>
2019-05-31 19:44:13 +0200

    feat(Giphy Api Example): gifs api get example

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,6 +1,20 @@
 import React, { Component } from 'react'
+import { giphyApi } from './../../services/api';
+
 class Home extends Component {
+
+  componentDidMount() {
+    giphyApi.get('', {
+      params: {
+        q: 'skate+tricks+nestor+judkins',
+      }
+    }).then(res => {
+      console.log(res);
+    }) 
+  }
+
   render() {
+    
     return (
       <React.Fragment>
         <h2>Home</h2>  
