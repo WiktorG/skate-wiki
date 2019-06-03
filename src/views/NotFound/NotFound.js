116dcfc120176eb10a18a53145fb7002e9719ad0
Wiktor Gała <wiktorgal4@gmail.com>
2019-06-03 13:50:14 +0200

    feat(404 page): 404 page styling and text change

--- src/views/NotFound/NotFound.js
+++ src/views/NotFound/NotFound.js
@@ -3,7 +3,14 @@ import React, { Component } from 'react'
 class NotFound extends Component {
   render() {
     return (
-        <h2>NotFound</h2>        
+        <h2 style={{
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%)',
+          textAlign: 'center',
+          width: '90%',
+        }}>404 - Woops..<br/>I didn't find the page you look for</h2>        
     )
   }
 }
