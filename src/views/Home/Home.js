930a089e91142858ea04b7ec024d3f3f745c0840
Wiktor Gała <wiktorgal4@gmail.com>
2019-06-03 21:41:42 +0200

    feat(Spinner, Gif): spinner add and gif star color change

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+
+import { Spin } from 'antd';
+
 import './../../components/Gifs/actions'
 
 import 'antd-mobile/lib/list-view/style/css';
+import 'antd/lib/spin/style/css';
 
 import { fetchHomeGifs } from './../../components/Gifs/actions'
 
@@ -17,7 +21,16 @@ class Home extends Component {
   render() {
     return (
       <div className="gif-list">
-        <GifList gifs={this.props.gifs.home.gifs} />
+        {this.props.gifs.home.fetching && this.props.gifs.home.gifs.length === 0 ? 
+        <Spin style={{
+          position: 'absolute',
+          left: '50%',
+          top: '50%',
+          transform: 'translate(-50%, -50%)',
+        }} />
+        :
+        <GifList gifs={this.props.gifs.home.gifs} />        
+        }
       </div>
     )
   }
